f5a460a52e301fc2c34cab170a57fcff6ab64d08
Aleksandr Babkin <bubkin-007@ya.ru>
2024-07-04 00:48:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 
+
+
+
+
+
   function onTelegramAuth(user) {
     alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
   }
